863bbf843a270f8583c2f23deae0096b153d8348
Alejandro Brunacci <Abrunacci@bitson.com.ar>
2018-01-19 09:34:50 -0300

    adding standings to admin

--- standings/admin.py
+++ standings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import General, Weekly
+
 
 # Register your models here.
+class GeneralAdmin(admin.ModelAdmin):
+    list_display = ('user', 'winner_hits', 'difference_hits', 'exact_hits', 'points')
+
+
+class WeeklyAdmin(admin.ModelAdmin):
+    list_display = ('week', 'user', 'winner_hits', 'difference_hits', 'exact_hits', 'points')
+
+
+admin.site.register(General, GeneralAdmin)
+admin.site.register(Weekly, WeeklyAdmin)
